7abf568652a9018a6a2a69938ed99c4107632e68
Jean Yang <bdlas321@gmail.com>
2019-02-18 17:19:22 +0800

    feat(teacher-page): basic layout & button component

---
+++ components/FlatButton.vue
@@ -0,0 +1,50 @@
+<template>
+    <button 
+        :class="'flat-button ' + size"
+        :style="{background: bgColor}"
+    >
+        <div class="text">{{text}}</div>
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        text: String,
+        bgColor: String,
+        size: {
+            default: 'medium',
+            validator: function (value) {
+                return ['small', 'medium', 'big'].indexOf(value) !== -1
+            }
+        },
+    },
+}
+</script>
+
+<style>
+.flat-button{
+    min-width: 81px;
+    height: 40px;
+    border: 3px solid lightgrey;
+    border-radius: 50px;
+    display: inline-block;
+    background-color: transparent;
+    color: lightgrey;
+    text-align: center;
+    font-size: 20px;
+    font-weight: 50;
+    line-height: 36px;
+    cursor: pointer;
+}
+.flat-button:focus{
+    outline: 0;
+}
+.big{
+    width: 200px;
+    height: 50px;
+    line-height: 45px;
+    font-size: 20px;
+    font-weight: 50;
+}
+</style>
---
+++ pages/teacher/index.vue
@@ -0,0 +1,102 @@
+<template>
+<body>
+  <section class="intro">
+      <div class="wrapper container">
+        <div class="row align-items-center">
+            <section class="course col-lg-6 col-sm-12 align-self-center">
+                <h3>你聽過安麗嗎？</h3>
+                <div class="actions">
+                    <flat-button size="big" text="課程介紹"/>
+                </div>
+            </section>
+            <section class="teacher col-lg-6 col-sm-12">
+                <div class="profile" />
+                <div class="name">您老師</div>
+                <div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis totam sapiente mollitia esse voluptatem, aliquid ea eius alias cum! Culpa numquam consequuntur dolore, velit accusamus porro ut modi doloribus aliquid praesentium laudantium corporis voluptate eos, doloremque iure, iusto mollitia quidem quae corrupti assumenda cum eveniet eligendi deserunt sed nemo.</div>
+            </section>
+        </div>
+    </div>
+  </section>
+  <section class="all-courses">
+      <h2 class="title">所有課程</h2>
+  </section>
+  <section class="course-intro">
+      <h2 class="title">課程簡介</h2>
+  </section>
+</body>
+</template>
+
+<script>
+import FlatButton from "~/components/FlatButton.vue";
+export default {
+    head() {
+        return {
+            link: [
+                { rel: 'stylesheet', href: '/bootstrap.css' },
+            ],
+        }
+    },
+  components: {
+    FlatButton
+  }
+};
+</script>
+
+<style scoped>
+* {
+  position: relative;
+}
+html,
+body {
+  padding: 0;
+  margin: 0;
+  height: 100%;
+}
+h2.title{
+    font-weight: 200;
+    font-size: 35px;
+    text-align: center;
+}
+.intro {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  min-height: 90vh;
+  background-color: #324d5b;
+  color: lightgrey;
+}
+.wrapper{
+    height: 90vh;
+}
+.row{
+    height: 100%;
+}
+.course{
+    text-align: center;
+}
+.course h3{
+    margin-bottom: 35px;
+    font-weight: 200;
+    font-size: 50px;
+}
+.teacher .profile{
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    background-image: url('https://via.placeholder.com/300.png');
+    background-position: center;
+}
+.teacher .name{
+    font-size: 25px;
+}
+.all-courses{
+    width: 100%;
+    background-color: white;
+    padding: 20px;
+}
+.course-intro{
+    width: 100%;
+    background-color: #F0F0F0;
+    padding: 20px;
+}
+</style>
