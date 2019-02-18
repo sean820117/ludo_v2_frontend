7c93080b4051976982245d87fb15789348783374
SeanGo <sean820117@gmail.com>
2019-02-18 15:03:16 +0800

    modify course v2

--- pages/course/index.vue
+++ pages/course/index.vue
@@ -60,10 +60,6 @@ import TempList from 'static/temp-list.png'
 export default {
     head () {
         return {
-            // script: [
-            //     { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js' },
-            //     { src: 'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/TweenMax.min.js' },
-            // ],
             link: [
                 { rel: 'stylesheet', href: '/bootstrap.css' }
             ]
