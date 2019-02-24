6896662df39b9622dd58d0cfcc49eb556d924968
Jean Yang <bdlas321@gmail.com>
2019-02-24 16:02:14 +0800

    feat(login-signup-page): page layout

---
+++ pages/login/index.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="body">
+    <page-header></page-header>
+    <main class="container">
+      <section class="title">
+        <h1>Log In</h1>
+        <p>New to Ludo? <a href="/signup">Sign Up</a></p>
+      </section>
+      <div class="row">
+        <section class="form col-lg-6 col-sm-12">
+          <div class="form-content">
+            <text-input name="email" text="Email" v-model="email" />
+            <text-input name="password" text="Password" type="password" v-model="password" />
+            <div class="error">{{errors}}</div>
+            <div>Forget Password?</div>
+            <div @click="onSubmit" role="button" class="action-button">Login</div>
+          </div>
+        </section>
+        <section class="third-party col-lg-6 col-sm-12">
+          <third-party-button platform="fb" />
+          <!-- <third-party-button platform="google" /> -->
+        </section>
+      </div>
+    </main>
+  </div>
+</template>
+<script>
+import PageHeader from '~/components/confirm/Header.vue'
+import TextInput from '~/components/TextInput.vue'
+import ThirdPartyButton from '~/components/ThirdPartyButton.vue'
+export default {
+  data() {
+    return {
+      errors: null,
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    onSubmit() {
+      // @TODO
+    },
+  },
+  head() {
+    return {
+      link: [{ rel: "stylesheet", href: "/bootstrap.css" }]
+    };
+  },
+  components: {
+    PageHeader,
+    TextInput,
+    ThirdPartyButton,
+  }
+};
+</script>
+<style scoped>
+.body {
+  box-sizing: border-box;
+  height: 100vh;
+}
+main {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.title {
+  margin-bottom: 5%;
+}
+h1 {
+  color: grey;
+  text-align: center;
+  font-size: 30px;
+  font-weight: normal;
+}
+.title p {
+  text-align: center;
+}
+.title a {
+  color: orange;
+  text-decoration: underline;
+}
+.form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center; 
+}
+.form-content {
+  width: 80%;
+}
+.third-party {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.third-party .third-party-button {
+  margin: 10px 0;
+}
+.action-button {
+  max-width: 150px;
+  margin: 30px auto;
+  padding: 10px;
+  border-radius: 10px;
+  background-color: #E0185D;
+  color: white;
+  text-align: center;
+  cursor: pointer;
+}
+@media (min-width: 900px) {
+  h1 {
+    font-size: 60px;
+  }
+  .action-button {
+    margin: 30px 0;
+  }
+  .action-button:hover {
+    transform: translate(0, -5%);
+  }
+}
+</style>
