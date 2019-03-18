db1b3f9f05669cc61c0e3a9c1ac56af66b646329
SeanGo <sean820117@gmail.com>
2019-03-18 12:36:49 +0800

    fixed 1st video error

--- static/data/course/06.js
+++ static/data/course/06.js
@@ -8,7 +8,7 @@ module.exports = {
             id:1,
             title:"簡短的精準自介(共同必修)",
             title2 : "開頭7秒讓教授留下印象！",
-            link:"https://player.vimeo.com/video/319273700",
+            link:"https://player.vimeo.com/video/319395957",
             assistant_id:"03db9c50-f211-11e8-93e5-7514b94dfe78",
             placeholder:"假設你今天是一個"+ "建築與設計學群" +"的申請生，請輸入一段簡短的自我介紹，目的是讓教授快速的認識你。(例如：我是林佳瑾，是一個關注心理健康的人，由於看見上百種不同的衝突都是源自於溝通不良，因此我期待能投入心理領域，在未來帶領大家從人心解決問題)",
             comments: {
