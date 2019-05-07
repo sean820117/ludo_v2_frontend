2866f03a9fb82e901394bb1c86175626c4e8a627
Chris <flyernaruto@gmail.com>
2019-05-08 03:04:21 +0800

    課程頁

---
+++ components/resume/CourseTitle.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="title-bar">
+        <div class="number-title">{{numberTitle}}</div>
+        <div class="course-title">{{courseTitle}}</div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        numberTitle: String,
+        courseTitle:String,
+    },
+}
+</script>
+<style>
+.title-bar{
+    margin: auto;
+    text-align: center;
+}
+.number-title{
+    font-size: 25px;
+    color: white;
+}
+.course-title{
+    font-size: 30px;
+    color: white;
+}
+</style>
---
+++ components/resume/QuestionBar.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="question-bar" v-if="toggled">
+        {{question}} 
+        <img class="cross" src="~/static/resume/path.png" @click="toggled = !toggled" />
+    </div>
+</template>
+<script>
+export default{
+    props:{
+        question: String,
+    },
+    data:() => ({
+        toggled: true,
+    }),
+}
+</script>
+<style>
+.question-bar{
+    position: relative;
+    width: 100%;
+    height: 33px;
+    line-height: 33px;
+    border-radius: 16px;
+    color: #007CDC;
+    background: #d9eeff;
+    padding-left: 20px;
+    font-size: 15px;
+    margin-bottom: 9px;
+}
+.cross{
+    position: absolute;
+    right: 20px;
+    top: 50%;
+    transform: translateY(-50%);
+}
+</style>
