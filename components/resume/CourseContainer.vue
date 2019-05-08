374d7bcc3d9837d177c5dcdf9b4ac152918ba5dc
Chris <flyernaruto@gmail.com>
2019-05-09 02:33:53 +0800

    下方三個標籤

---
+++ components/resume/CourseContainer.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="course-menu-container">
+        <input class="labels first-radio" type="radio" :name="labelName" :id="labelName+'-first'" checked/>
+        <input class="labels second-radio" type="radio" :name="labelName" :id="labelName+'-second'" />
+        <input class="labels third-radio" type="radio" :name="labelName" :id="labelName+'-third'" />
+
+        <div class="course-menu">
+            <div class="threee-labels">
+                <label class="first-label" :for="labelName+'-first'">課程選單</label>
+                <label class="second-label" :for="labelName+'-second'">AI 評測</label>
+                <label class="third-label" :for="labelName+'-third'">下載資源</label>
+            </div>
+
+            <div>
+                <div class="first-content">
+                    <slot name="first-content"></slot>
+                </div>
+                <div class="second-content">
+                    <slot name="second-content"></slot>
+                </div>
+                <div class="third-content">
+                    <slot name="third-content"></slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default{
+    props:{
+        labelName: String,
+    },
+}
+</script>
+<style>
+.course-menu-container{
+    box-sizing: border-box;
+    width: 100%;
+    border-left: solid rgba(0,0,0,0) 25px;
+    border-right: solid rgba(0,0,0,0) 25px;
+    height: 100%;
+}
+.labels{
+    display: none;
+}
+.course-menu{
+    display: grid;
+    grid-template-rows: 20px auto;
+    height: 100%;
+    color: white;
+    font-size: 12px;
+    font-weight: 500;
+    border-top: solid white 1px;
+}
+.threee-labels{
+    display: grid;
+    grid-template-columns: 33% 33% auto;
+    text-align: center;
+}
+.threee-labels label{
+    border-top: solid rgba(0,0,0,0) 4px;
+    margin-top: -1px;
+    padding-top: 5px;
+}
+.first-radio:checked ~ .course-menu .first-label,
+.second-radio:checked ~ .course-menu .second-label,
+.third-radio:checked ~ .course-menu .third-label{
+    border-top: solid white 4px;
+}
+</style>
