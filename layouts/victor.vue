ed97d1f52d1ec484243a09315ada1bad9b575965
SeanGo <sean820117@gmail.com>
2019-05-23 22:44:47 +0800

    add victor og img

--- layouts/victor.vue
+++ layouts/victor.vue
@@ -59,12 +59,12 @@ export default {
             meta: [
                 { charset: 'utf-8' },
                 { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-                { hid: 'description', name: 'description', content: '得勝者文教，帶你秒懂108課綱—學習歷程' },
+                { hid: 'description', name: 'description', content: '得勝者文教，帶你秒懂108課綱—學習歷程！10堂課程免費看，立即註冊馬上拿！' },
                 { property : 'og:title' , content:"學習歷程 - 得勝者文教"},
                 { property : 'og:type' , content:"education tech."},
                 { property : 'og:url' , content:"https://www.ludonow.com/victor"},
-                { property : 'og:image' , content:""},
-                { property : 'og:description' , content:"得勝者文教，帶你秒懂108課綱—學習歷程"},
+                { property : 'og:image' , content:"/victor-og-img.jpg"},
+                { property : 'og:description' , content:"得勝者文教，帶你秒懂108課綱—學習歷程！10堂課程免費看，立即註冊馬上拿！"},
                 { property : 'og:site_name' , content:"www.ludonow.com"},
             ],
             link: [
