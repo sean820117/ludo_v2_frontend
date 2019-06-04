646d2d554955eadc9f72e85c5aefbe0f0f9436eb
Ludo <sean820117@gmail.com>
2019-06-04 11:32:23 +0800

    Minor modfied.

--- pages/fitness/demo.vue
+++ pages/fitness/demo.vue
@@ -3,17 +3,33 @@
         <course-header bgColor="transparent" color="#76FF00" />
         <practice-area></practice-area>
     </div>
-    
+
 </template>
 
 <script>
 import CourseHeader from '~/components/CourseHeader.vue';
 import PracticeArea from '~/components/fitness/PracticeArea.vue';
+
+import Vuex from 'vuex';
+import Vue from 'vue';
+import VueMq from 'vue-mq';
+
+Vue.use(VueMq, {
+  breakpoints: {
+    // small: 400,
+    mobile: 992,
+    desktop: Infinity,
+  }
+});
+
 export default {
     components: {
         CourseHeader,
         PracticeArea,
-    }
+    },
+    data:()=> ({
+        is_data_fetched:false,
+    }),
 }
 </script>
 
