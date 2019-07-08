5c80e9b9fcec8120b1b65e29e772130fb78167ca
SeanGo <sean820117@gmail.com>
2019-07-08 17:19:42 +0800

    catch no pose error for teach page

--- pages/mommiyoga/teach.vue
+++ pages/mommiyoga/teach.vue
@@ -153,33 +153,41 @@ export default {
             form.append('file',e.target.files[0])
             form.append('pose_id','yoga_6')
             form.append('language','zh-tw')
-            const res = await axios.post('/apis/video-upload',form)
-            console.log(res.data)
-            for(var i =0; i< res.data.reps_wrong_tags.length; i++){
-              for(var j = 0; j<res.data.reps_wrong_tags[i].length; j++){
-                  if(res.data.reps_wrong_tags[i][j] == "y_6_1") res.data.reps_wrong_tags[i][j] = "膝盖弯曲";
-                  else if (res.data.reps_wrong_tags[i][j] == "y_6_2") res.data.reps_wrong_tags[i][j] = "膝盖弯曲";
-                  else if (res.data.reps_wrong_tags[i][j] == "y_6_3") res.data.reps_wrong_tags[i][j] = "抬腿速度太快";
-                  else if (res.data.reps_wrong_tags[i][j] == "y_6_4") res.data.reps_wrong_tags[i][j] = "抬腿速度太快";
-                  else if (res.data.reps_wrong_tags[i][j] == "y_6_5") res.data.reps_wrong_tags[i][j] = "轴心不稳";
-                  else if (res.data.reps_wrong_tags[i][j] == "correct") res.data.reps_wrong_tags[i][j] = "姿势正确";
-              }
+            try {
+                const res = await axios.post('/apis/video-upload',form)
+                console.log(res.data)
+                if(res.data.status == 500) {
+                    alert('未偵測到動作或人物');
+                } else {
+                    for(var i =0; i< res.data.reps_wrong_tags.length; i++){
+                        for(var j = 0; j<res.data.reps_wrong_tags[i].length; j++){
+                            if(res.data.reps_wrong_tags[i][j] == "y_6_1") res.data.reps_wrong_tags[i][j] = "膝盖弯曲";
+                            else if (res.data.reps_wrong_tags[i][j] == "y_6_2") res.data.reps_wrong_tags[i][j] = "膝盖弯曲";
+                            else if (res.data.reps_wrong_tags[i][j] == "y_6_3") res.data.reps_wrong_tags[i][j] = "抬腿速度太快";
+                            else if (res.data.reps_wrong_tags[i][j] == "y_6_4") res.data.reps_wrong_tags[i][j] = "抬腿速度太快";
+                            else if (res.data.reps_wrong_tags[i][j] == "y_6_5") res.data.reps_wrong_tags[i][j] = "轴心不稳";
+                            else if (res.data.reps_wrong_tags[i][j] == "correct") res.data.reps_wrong_tags[i][j] = "姿势正确";
+                        }
+                    }
+                    this.reps_wrong_tags = res.data.reps_wrong_tags;
+                    this.video_result = res.data;
+                    
+                    // setTimeout(()=> {
+                    //     clearInterval(this.interval);
+                    //     this.is_uploading = false;
+                    //     this.value = 0;
+                    //     this.is_showing = true;
+                    // }, 1000)
+                    // setTimeout(()=> {
+                    //     this.$scrollTo('#result-box',"start");
+                    // },1500)
+                    // window.alert('Done');
+                }
+            } catch (error) {
+                alert("網路錯誤或其他錯誤");
             }
             this.isLoading = false;
-            console.log(res.data)
-            this.reps_wrong_tags = res.data.reps_wrong_tags;
-            this.video_result = res.data;
             this.is_loaded = true;
-            // setTimeout(()=> {
-            //     clearInterval(this.interval);
-            //     this.is_uploading = false;
-            //     this.value = 0;
-            //     this.is_showing = true;
-            // }, 1000)
-            // setTimeout(()=> {
-            //     this.$scrollTo('#result-box',"start");
-            // },1500)
-            // window.alert('Done');
         },
         handleRetryEvent(e){
             console.log("ok");
