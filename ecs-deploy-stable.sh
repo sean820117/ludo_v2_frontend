1f259af3aff35db1e6d1a4b73f0ff8fa396b3c48
SeanGo <sean820117@gmail.com>
2019-07-15 14:36:58 +0800

    change deploy stable shell to jp

--- ecs-deploy-stable.sh
+++ ecs-deploy-stable.sh
@@ -1,11 +1,11 @@
 echo "ecr get login"
-$(aws ecr get-login --no-include-email --region ap-southeast-1)
+$(aws ecr get-login --no-include-email --region ap-northeast-1)
 echo "build"
 docker build -t ludo-frontend .
 echo "tag"
-docker tag ludo-frontend:latest 216891314812.dkr.ecr.ap-southeast-1.amazonaws.com/ludo-frontend:stable
+docker tag ludo-frontend:latest 216891314812.dkr.ecr.ap-northeast-1.amazonaws.com/ludo-frontend:latest
 echo "push"
-docker push 216891314812.dkr.ecr.ap-southeast-1.amazonaws.com/ludo-frontend:stable
+docker push 216891314812.dkr.ecr.ap-northeast-1.amazonaws.com/ludo-frontend:latest
 sleep 2m
 echo "deploy"
 # aws ecs update-service --cluster ludo-frontend-cluster-v1  --service Ludo-frontend-container-service --force-new-deployment
