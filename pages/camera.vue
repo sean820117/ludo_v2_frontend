f00d17a74980052c161bb1277dd91bce12d8191a
SeanGo <sean@ludonow.com>
2019-09-17 09:57:08 +0000

    test new camera type.

--- pages/camera.vue
+++ pages/camera.vue
@@ -2,17 +2,24 @@
     <div class="container">
         <div class="card">
         <div class="card-body">
-            <video id="inputVideo" alt="在這裡錄影" muted>Video stream not available.</video>
-            <div id="course-video-container">
-                <video loop controls autoplay playsinline muted  class="index-wrap-video" poster="https://ludo-beta.s3-ap-southeast-1.amazonaws.com/static/mommiyoga/index-wrap-video-preview.png">
-                    <source src="https://ludo-beta.s3-ap-southeast-1.amazonaws.com/static/mommiyoga/index-wrap-video.mp4">
-                </video>
+            <div id="input-video-container">
+                <video id="inputVideo" alt="在這裡錄影" muted>Video stream not available.</video>
             </div>
+            <!-- <div id="course-video-container">
+                <video loop controls autoplay playsinline muted  class="index-wrap-video" poster="https://ludo-beta.s3-ap-southeast-1.amazonaws.com/static/mommiyoga/index-wrap-video-preview.png">
+                    <source src="https://ludo-beta.s3-ap-southeast-1.amazonaws.com/static/mommiyoga/mamiyoga-course/mami_yoga_L13-3_cn.mp4">
+                </video>
+            </div> -->
             <div class="row mb-4 justify-content-center align-items-center">
-            <div class="col-4 d-flex justify-content-center align-items-center">
+            <!-- <div class="col-4 d-flex justify-content-center align-items-center">
                 <button id="startBtn" class="btn btn-sm btn-outline-primary">Start Recording</button>
                 <button id="stopBtn" class="btn btn-sm btn-outline-danger">Stop Recording</button>
                 <button id="resetBtn" class="btn btn-sm btn-outline-info">Restart Recorder</button>
+            </div> -->
+            <div class="btn-style">
+                <button @click="recordVideo" id="startBtn" class="btn btn-sm btn-outline-primary">▶</button>
+                <button @click="stopRecord" id="stopBtn" class="btn btn-sm btn-outline-danger">■</button>
+                <button @click="retryRecord" id="resetBtn" class="btn btn-sm btn-outline-info">⏎</button>
             </div>
             </div>
 
@@ -45,7 +52,7 @@ export default {
                 "mandatory": {
                     "maxWidth": 640,
                     "maxHeight": 480,
-                    "maxFrameRate": 30,
+                    "maxFrameRate": 12,
                 },
             }
         },
@@ -55,12 +62,12 @@ export default {
     async mounted() {
         if (process.client) {
             this.inputVideo = document.querySelector('#inputVideo')
-            this.outputVideo = document.querySelector('#outputVideo')
+            // this.outputVideo = document.querySelector('#outputVideo')
 
             // <button> element
-            this.startBtn = document.querySelector('#startBtn')
-            this.stopBtn = document.querySelector('#stopBtn')
-            this.resetBtn = document.querySelector('#resetBtn')
+            // this.startBtn = document.querySelector('#startBtn')
+            // this.stopBtn = document.querySelector('#stopBtn')
+            // this.resetBtn = document.querySelector('#resetBtn')
 
             // error message
             this.errorElement = document.querySelector('#errorMsg')
@@ -71,35 +78,68 @@ export default {
             this.video_recorder = await this.$videoRecorder();
             console.log(this.video_recorder);
             this.video_recorder.openCamera(this.constraints,this.inputVideo);
-            this.startBtn.addEventListener('click', this.video_recorder.startRecording)
-            this.stopBtn.addEventListener('click', this.video_recorder.stopRecording)
-            this.resetBtn.addEventListener('click', this.video_recorder.closeCamera)
+            // this.startBtn.addEventListener('click', this.video_recorder.startRecording)
+            // this.stopBtn.addEventListener('click', this.video_recorder.stopRecording)
+            // this.resetBtn.addEventListener('click', this.video_recorder.closeCamera)
             // this.mediaRecorderSetup();
             
         }
     },
     methods:{
-        
+        recordVideo(){
+            this.video_recorder.startRecording
+            console.log('recording')
+        },
+        stopRecord(){
+            this.video_recorder.stopRecording
+            console.log('stop')
+        },
+        retryRecord(){
+            this.video_recorder.closeCamera;
+            console.log('retry')
+        }
+
     },
 }
 </script>
 
 <style>
-#inputVideo {
+/* #inputVideo {
     width: 150px;
     height: 200px;
     transform: scaleX(-1);
     z-index: 10000;
     position: absolute;
+} */
+#input-video-container {
+    /* width: 100vh;
+    height: 100vw; */
+    margin-top: 8vh;
+    transform: rotate(90deg);
 }
+#inputVideo {
+    height:100vw;
+}
+
+
 #course-video-container {
-    width: 100vh;
-    height: 100vw;
+    /* width: 100vh;
+    height: 100vw; */
+    margin-top: 8vh;
     transform: rotate(90deg);
 }
 .index-wrap-video {
-    width: 100%;
-    height: 100%;
-    
+    /* width: 100%;
+    height: 100%; */
+    height: 100vw;
+}
+
+.btn-style{
+    position: fixed;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    bottom: 15vh;
+    left: 50%;
 }
 </style>
