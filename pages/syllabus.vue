38accc1177b136a49a759350874d8592f6872c8b
SeanGo <sean820117@gmail.com>
2019-10-21 00:18:43 +0800

    add trial over alert

--- pages/syllabus.vue
+++ pages/syllabus.vue
@@ -343,7 +343,9 @@ export default {
                 let use_time = (now_time - open_time)/86400000;
                 console.log(use_time)
                 if(use_time > 7){
-                    this.have_trial = false;                                               
+                    this.have_trial = false;   
+                    alert('已超過試用期限，請前往購買或聯繫客服由我們為您專人服務呦～')
+                    this.$router.push('/');
                 }else {
                     this.have_trial = true;
                 }
